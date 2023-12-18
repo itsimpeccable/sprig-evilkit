68ac829e0c26188bc9627d3d0c323535f03e90af
Doruk Sarp Aydın <51460403+itsimpeccable@users.noreply.github.com>
2023-12-18 22:03:06 +0300

    Delete pico-badusb directory

--- pico-badusb/boot.py
+++
@@ -1,5 +0,0 @@
-from badusb.boot import Boot
-
-# Loads default device setup
-if __name__ == "__main__":
-    Boot()
--- pico-badusb/main.py
+++
@@ -1,5 +0,0 @@
-from badusb.command import Command
-
-# Loads and executes commands from the payload
-if __name__ == "__main__":
-    Command().execute("./payload.txt")
